fbe07f374fe1eb9778b0bb914e5d8a37068d492c
wael656 <waelalshamaly@gmail.com>
2025-01-21 09:05:01 +0100

    connect

--- bloc2_WAELALSHAMALY/postgresql_python/connect.py
+++ bloc2_WAELALSHAMALY/postgresql_python/connect.py
@@ -1,12 +1,12 @@
 import psycopg2
 
-def cpnnection_db():
-        conn = psycopg2.connect(
-            database="the_bear",
-            password="admin",
-            user="admin",
-            host="localhost",
-            port="5432"
-        )
-    
-    return conn
+def connection_db():
+    conn = psycopg2.connect(
+        database="the_bear",
+        password="admin",
+        user="admin",
+        host="localhost",
+        port="5432"
+    )
+
+return conn
